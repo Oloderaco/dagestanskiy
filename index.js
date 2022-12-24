9fcb54330ab802b128fb2ca536d10358b0b1f5dc
Mikky <Ruzievmirfajz@gmail.com>
2022-12-24 21:22:10 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+var firstname;
+var age;
+alert(' Ответьте на вопросы правильно !')
+var pojizni = prompt(' а вы не робот ? ')
+alert('Если не ' + pojizni + ' то кто?');
+var firstname = prompt(' Как вас зовут ? ');
+var age = +prompt(' Вам сколько лет ? ');
+console.log('вас зовут ' + firstname + ' и вам ' + age + ' лет');
+alert(' Все брат , тормози леее  ');
+alert(' а ты математику любишь ? ');
+alert(' так и знал ))) ');
+var umno = +prompt(' 10 * 5 = ')
+console.log('правильный ответ > 10 + 5 = ' + (10 + 5) + " A ваш ответ > " + umno);
+var geniy = +prompt(' 10 * 5 = ')
+console.log("правильный ответ > 10 * 5 = " + 10 * 5 + " A ваш ответ > " + geniy);
+var molodes = +prompt(' 10 / 5 = ')
+console.log("правильный ответ > 10 / 5 = " + 10 / 5 + " A ваш ответ > " + molodes);
+var krasava = +prompt(' 16 % 5 = ')
+console.log("правильный ответ > 16 % 5 = " + 16 % 5 + " A ваш ответ > " + krasava);
+alert('Ладно, Ща фокус покажу');
+var x = +prompt("Загадай первое число ," + firstname);
+var y = +prompt(' теперь второе');
+var z = +prompt(' и третье');
+alert((x + y + z) / 3);
+alert(' лее проверь да Console');
